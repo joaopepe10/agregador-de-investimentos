d50d03e2c3dadfd5fbf22df068e75d9bedf24188
João Perdro Pires <joaopedropires61@gmail.com>
2024-01-06 15:12:29 -0300

    Inicializando repositório remoto

---
+++ src/main/java/io/github/joaopepe10/agregadordeinvestimentos/AgregadorDeInvestimentosApplication.java
@@ -0,0 +1,13 @@
+package io.github.joaopepe10.agregadordeinvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorDeInvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadorDeInvestimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/joaopepe10/agregadordeinvestimentos/AgregadorDeInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaopepe10.agregadordeinvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorDeInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
